a3a421867725bebae68d91ec36c99a4f414cefe6
houjie_qx <houjie_qx@piesat.cn>
2021-07-20 17:48:33 +0800

    修改连接池未生效问题

--- src/main/java/cn/piesat/rediszsetq/config/BeanConfig.java
+++ src/main/java/cn/piesat/rediszsetq/config/BeanConfig.java
@@ -8,15 +8,21 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -29,8 +35,36 @@ public class BeanConfig {
 
     private static final Logger log = LoggerFactory.getLogger(BeanConfig.class);
 
+    @Bean("zsetQLettuceConnectionFactory")
+    public LettuceConnectionFactory lettuceConnectionFactory(RedisProperties redisProperties) {
+        RedisProperties.Lettuce lettuce = redisProperties.getLettuce();
+        GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+        genericObjectPoolConfig.setMaxIdle(lettuce.getPool().getMaxIdle());
+        genericObjectPoolConfig.setMinIdle(lettuce.getPool().getMinIdle());
+        genericObjectPoolConfig.setMaxTotal(lettuce.getPool().getMaxActive());
+        genericObjectPoolConfig.setMaxWaitMillis(lettuce.getPool().getMaxWait().toMillis());
+        genericObjectPoolConfig.setTimeBetweenEvictionRunsMillis(100);
+
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setDatabase(redisProperties.getDatabase());
+        redisStandaloneConfiguration.setHostName(redisProperties.getHost());
+        redisStandaloneConfiguration.setPort(redisProperties.getPort());
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(redisProperties.getPassword()));
+
+        LettuceClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
+                .commandTimeout(redisProperties.getTimeout())
+                .shutdownTimeout(lettuce.getShutdownTimeout())
+                .poolConfig(genericObjectPoolConfig)
+                .build();
+
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfig);
+//        factory.setShareNativeConnection(true);
+//        factory.setValidateConnection(false);
+        return factory;
+    }
+
     @Bean("zsetQRedisTemplate")
-    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(@Qualifier("zsetQLettuceConnectionFactory") LettuceConnectionFactory connectionFactory) {
         log.info("-----------> create zsetq redisTemplate bean...");
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
